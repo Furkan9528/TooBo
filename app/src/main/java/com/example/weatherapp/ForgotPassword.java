16f392ffbc04444a82ccf90c096809e1ab764875
Furkan KARA <furkankara.9528@gmail.com>
2021-12-19 23:33:18 +0100

    Weather Application

--- app/src/main/java/com/example/weatherapp/ForgotPassword.java
+++ app/src/main/java/com/example/weatherapp/ForgotPassword.java
@@ -1,15 +1,44 @@
 package com.example.weatherapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ForgotPassword extends AppCompatActivity {
 
+
+    private EditText emailEditText;
+    private Button resetPasswordButton;
+    private ProgressBar progressBar;
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgotpassword);
+
+        emailEditText = (EditText) findViewById(R.id.email);
+        resetPasswordButton = (Button) findViewById(R.id.resetPassword);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        auth = FirebaseAuth.getInstance();
+
+        resetPasswordButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                resetPassword();
+            }
+        });
+    }
+    private void resetPassword(){
+
     }
 }
